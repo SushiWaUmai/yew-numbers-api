b8e4643aee801b9275942c0e08eeb7a6c972c15b
Eugene Matsumura <eugene.bb1@gmail.com>
2023-02-12 13:55:50 +0100

    feat: :sparkles: style the fact page

--- src/pages/fact.rs
+++ src/pages/fact.rs
@@ -1,5 +1,8 @@
 use gloo::net::http::Request;
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::Route;
 
 #[derive(Properties, PartialEq)]
 pub struct FactProps {
@@ -9,9 +12,9 @@ pub struct FactProps {
 #[function_component(Fact)]
 pub fn fact(props: &FactProps) -> Html {
     let num_fact = use_state(|| "".to_owned());
+    let number = props.number;
     {
         let num_fact = num_fact.clone();
-        let number = props.number;
 
         use_effect_with_deps(
             move |_| {
@@ -28,13 +31,28 @@ pub fn fact(props: &FactProps) -> Html {
                     num_fact.set(fetched_fact);
                 });
             },
-            (),
+            number,
         )
     }
 
     html! {
-        <div class={classes!("container", "mx-auto")}>
-            <h1 class={classes!("text-6xl", "text-center")}>{num_fact.as_str()}</h1>
+        <div class={classes!("h-full")}>
+            <div class={classes!("container", "mx-auto", "h-full", "flex", "flex-col")}>
+                <div class={classes!("flex-grow", "grid", "place-items-center", "h-full")}>
+                    <div>
+                        <h1 class={classes!("text-8xl", "text-tiffany", "text-center")}>{number}</h1>
+                        <p class={classes!("text-2xl", "text-center")}>{num_fact.as_str()}</p>
+                    </div>
+                </div>
+                <div class={classes!("flex", "justify-center", "gap-x-4", "m-8")}>
+                    <Link<Route> to={Route::Fact { number: number - 1 }}>
+                        <span class={classes!("text-2xl", "bg-jet", "px-4", "py-2", "rounded")}>{"<"}</span>
+                    </Link<Route>>
+                    <Link<Route> to={Route::Fact { number: number + 1 }}>
+                        <span class={classes!("text-2xl", "bg-jet", "px-4", "py-2", "rounded")}>{">"}</span>
+                    </Link<Route>>
+                </div>
+            </div>
         </div>
     }
 }
